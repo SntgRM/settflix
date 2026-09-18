1acaca01680da7d2a755305392726d3c883d669f
SntgRM <santy01052007@gmail.com>
2026-09-18 14:59:33 -0500

    REF - refactor SkeletonCard component to replace background color with skeleton class for improved loading effect

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -9,9 +9,9 @@ const Loader = ({ label = 'Cargando...' }) => (
 
 export const SkeletonCard = () => (
   <div className="flex-shrink-0 w-[140px] md:w-[180px]">
-    <div className="aspect-[2/3] bg-[#161616] rounded-lg animate-pulse" />
-    <div className="mt-2 h-3 w-3/4 bg-[#161616] rounded animate-pulse" />
-    <div className="mt-1 h-2 w-1/3 bg-[#161616] rounded animate-pulse" />
+    <div className="aspect-[2/3] skeleton rounded-lg" />
+    <div className="mt-2 h-3 w-3/4 skeleton rounded" />
+    <div className="mt-1 h-2 w-1/3 skeleton rounded" />
   </div>
 );
 
