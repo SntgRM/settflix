3c738cf9e8c0ecd0f368e70d8318c986d162e726
SntgRM <santy01052007@gmail.com>
2026-09-18 14:54:05 -0500

    REF - refactor App component to use AppLayout and integrate FavoritesProvider for improved structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ToastProvider } from './context/ToastContext';
+import { FavoritesProvider } from './context/FavoritesContext';
 import PrivateRoute from './routes/PrivateRoute';
-import Navbar from './components/Navbar';
+import AppLayout from './components/AppLayout';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
@@ -14,30 +15,24 @@ function App() {
   return (
     <AuthProvider>
       <ToastProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route
-              path="/"
-              element={
-                <PrivateRoute>
-                  <Navbar searchQuery={searchQuery} onSearch={setSearchQuery} />
-                  <Home searchQuery={searchQuery} />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/favorites"
-              element={
-                <PrivateRoute>
-                  <Navbar searchQuery={searchQuery} onSearch={setSearchQuery} />
-                  <Favorites />
-                </PrivateRoute>
-              }
-            />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </BrowserRouter>
+        <FavoritesProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route element={<PrivateRoute />}>
+                <Route
+                  element={
+                    <AppLayout searchQuery={searchQuery} onSearch={setSearchQuery} />
+                  }
+                >
+                  <Route path="/" element={<Home searchQuery={searchQuery} />} />
+                  <Route path="/favorites" element={<Favorites />} />
+                </Route>
+              </Route>
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </BrowserRouter>
+        </FavoritesProvider>
       </ToastProvider>
     </AuthProvider>
   );
