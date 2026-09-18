13f14c8029914b07a05345e2f7c51e50f320956b
SntgRM <santy01052007@gmail.com>
2026-09-17 19:41:09 -0500

    ADD - implement authentication URLs for login and token refresh

---
+++ server/authentication/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import LoginView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path('login/', LoginView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
+]
