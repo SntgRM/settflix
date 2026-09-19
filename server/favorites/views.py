dff4f121b593c2afbf5f8c450e611257684a7029
SntgRM <santy01052007@gmail.com>
2026-09-18 21:16:11 -0500

    ADD - enhance Favorita creation with movie details retrieval and error handling

--- server/favorites/views.py
+++ server/favorites/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from django.db import IntegrityError
 from .models import Favorita
 from .serializers import FavoritaSerializer, FavoritaUpdateSerializer
+from movies.services import obtener_pelicula_por_id, OMDbServiceError, OMDbNotFoundError
 
 class FavoritaListCreateView(generics.ListCreateAPIView):
     
@@ -17,15 +18,42 @@ class FavoritaListCreateView(generics.ListCreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
+        id_pelicula = serializer.validated_data['id_pelicula']
+
         try:
-            serializer.save(usuario=request.user)
+            pelicula = obtener_pelicula_por_id(id_pelicula)
+        except OMDbNotFoundError as exc:
+            return Response(
+                {'error': str(exc)},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        except OMDbServiceError as exc:
+            return Response(
+                {'error': str(exc)},
+                status=status.HTTP_502_BAD_GATEWAY
+            )
+
+        try:
+            favorita = Favorita.objects.create(
+                usuario=request.user,
+                id_pelicula=pelicula['id_pelicula'],
+                titulo=pelicula['titulo'],
+                anio=pelicula['anio'],
+                poster=pelicula['poster'],
+                nota=serializer.validated_data.get('nota'),
+            )
         except IntegrityError:
             return Response(
                 {'error': 'Esta película ya está en tus favoritas.'},
                 status=status.HTTP_409_CONFLICT
             )
 
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        output_serializer = self.get_serializer(favorita)
+
+        return Response(
+            output_serializer.data,
+            status=status.HTTP_201_CREATED
+        )
 
 class FavoritaDetailView(generics.RetrieveUpdateDestroyAPIView):
     
