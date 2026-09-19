81f3b2f81469ff6534e71e859d1cb247bda2240b
SntgRM <santy01052007@gmail.com>
2026-09-18 21:15:06 -0500

    ADD - implement logout endpoint in authentication URLs for user session management

--- server/authentication/urls.py
+++ server/authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, LogoutView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
 ]
